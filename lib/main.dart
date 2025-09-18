d5e4f1e8c57131cc77a0dc05b27c3354bb751967
VICTUS <bishal.tmg056@gmail.com>
2025-09-18 11:51:48 +0545

    dahboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+
 import 'package:flutter/material.dart';
-import 'package:flutterproject/login.dart';
+import 'dashboard.dart';
 import 'demoA.dart';
+import 'dash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +15,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PCPS DEMO',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: LoginPage(),
+      home:  dashboard2(),
     );
   }
 }
-
-
